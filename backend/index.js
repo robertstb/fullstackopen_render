3379764485c94996df79d48ef44f94b02c0805fa
robertstb <robert.stenbacka@gmail.com>
2025-10-14 21:47:53 +0300

    Removed get path '/'

--- backend/index.js
+++ backend/index.js
@@ -42,9 +42,7 @@ app.use(cors())
 app.use(requestLogger)
 
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+
 
 app.get('/api/notes', (request, response) => {
   response.json(notes)
